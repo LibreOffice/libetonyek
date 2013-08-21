0853e8b3bad8b23d4eb35e48c17f3be25a9e7d47
David Tardon <dtardon@redhat.com>
2013-08-21 20:05:04 +0200

    move parsing of styles to an extra class

---
+++ src/lib/KN2StyleParser.h
@@ -0,0 +1,44 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/*
+ * This file is part of the libkeynote project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef KN2STYLEPARSER_H_INCLUDED
+#define KN2STYLEPARSER_H_INCLUDED
+
+#include "KNStyle_fwd.h"
+
+namespace libkeynote
+{
+
+class KNCollector;
+class KNXMLReader;
+
+class KN2StyleParser
+{
+public:
+  KN2StyleParser(int nameId, int nsId, KNCollector *collector);
+
+  void parse(const KNXMLReader &reader);
+
+  const KNStylePtr_t &getStyle() const;
+
+private:
+  void parsePropertyMap(const KNXMLReader &reader);
+
+private:
+  const int m_nameId;
+  const int m_nsId;
+  KNCollector *const m_collector;
+  KNStylePtr_t m_style;
+};
+
+}
+
+#endif // KN2STYLEPARSER_H_INCLUDED
+
+/* vim:set shiftwidth=2 softtabstop=2 expandtab: */
