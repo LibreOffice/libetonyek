58b40cc300fb04c7a3c2f2f0b412fd72caca472a
David Tardon <dtardon@redhat.com>
2013-09-11 14:06:17 +0200

    add unit tests (empty now) for KNPath

---
+++ src/test/KNPathTest.cpp
@@ -0,0 +1,41 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/*
+ * This file is part of the libkeynote project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include "KNPath.h"
+
+#include "KNPathTest.h"
+
+using libkeynote::KNPath;
+
+namespace test
+{
+
+void KNPathTest::setUp()
+{
+}
+
+void KNPathTest::tearDown()
+{
+}
+
+void  KNPathTest::testConstruction()
+{
+  // TODO: implement me
+}
+
+void KNPathTest::testConstructionFromString()
+{
+  // TODO: implement me
+}
+
+CPPUNIT_TEST_SUITE_REGISTRATION(KNPathTest);
+
+}
+
+/* vim:set shiftwidth=2 softtabstop=2 expandtab: */
