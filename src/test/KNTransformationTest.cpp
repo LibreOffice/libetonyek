e5f60c6066086fcc6320cda542087aa02e5b0b29
David Tardon <dtardon@redhat.com>
2013-09-13 18:40:21 +0200

    add dummy test for transformations

---
+++ src/test/KNTransformationTest.cpp
@@ -0,0 +1,37 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/*
+ * This file is part of the libkeynote project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include "KNTransformation.h"
+
+#include "KNTransformationTest.h"
+
+namespace test
+{
+
+void KNTransformationTest::setUp()
+{
+}
+
+void KNTransformationTest::tearDown()
+{
+}
+
+void KNTransformationTest::testConstruction()
+{
+  // TODO: implement me
+}
+
+void KNTransformationTest::testMultiplication()
+{
+  // TODO: implement me
+}
+
+}
+
+/* vim:set shiftwidth=2 softtabstop=2 expandtab: */
