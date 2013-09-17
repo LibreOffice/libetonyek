179180d6167e4e00edda227aa445a5d1fcc0be20
David Tardon <dtardon@redhat.com>
2013-09-17 17:59:23 +0200

    draw polygon

--- src/lib/KNShape.cpp
+++ src/lib/KNShape.cpp
@@ -7,19 +7,133 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
+#include <cmath>
+#include <deque>
+
+#include <boost/bind.hpp>
+
 #include "KNPath.h"
 #include "KNShape.h"
+#include "KNTransformation.h"
 #include "KNTypes.h"
+
+using std::deque;
 
 namespace libkeynote
 {
 
-KNPathPtr_t makePolygonPath(const KNSize &size, const unsigned edges)
+namespace
 {
-  // TODO: implement me
-  (void) size;
-  (void) edges;
-  return KNPathPtr_t();
+
+struct Point
+{
+  double x;
+  double y;
+
+  Point();
+  Point(double x_, double y_);
+};
+
+Point::Point()
+  : x(0)
+  , y(0)
+{
+}
+
+Point::Point(const double x_, const double y_)
+  : x(x_)
+  , y(y_)
+{
+}
+
+bool operator==(const Point &left, const Point &right)
+{
+  return (left.x == right.x) && (left.y == right.y);
+}
+
+bool operator!=(const Point &left, const Point &right)
+{
+  return !(left == right);
+}
+
+}
+
+namespace
+{
+
+using namespace transformations;
+
+deque<Point> rotatePoint(const Point &point, const unsigned n)
+{
+  deque<Point> points;
+
+
+  const double angle = 2 * M_PI / n;
+
+  points.push_back(point);
+  for (unsigned i = 1; i < n; ++i)
+  {
+    Point pt(point);
+    const KNTransformation rot(rotate(i * angle));
+    rot(pt.x, pt.y);
+    points.push_back(pt);
+  }
+
+  return points;
+}
+
+KNPathPtr_t makePolyLine(const deque<Point> inputPoints, bool close = true)
+{
+  KNPathPtr_t path;
+
+// need at least 2 points to make a polyline
+  if (inputPoints.size() < 2)
+    return path;
+
+  // remove multiple points
+  deque<Point> points;
+  {
+    deque<Point>::const_iterator first = inputPoints.begin();
+    deque<Point>::const_iterator last = first;
+    do
+    {
+      last = adjacent_find(first, inputPoints.end());
+      copy(first, last, back_inserter(points));
+      // skip the range of adjacent, equal points
+      first = adjacent_find(last, inputPoints.end(), boost::bind(operator!=, _1, _2));
+      if (inputPoints.end() != first)
+        ++first;
+    }
+    while (first != inputPoints.end());
+  }
+
+  // close path if the first and last points are equal
+  if (points.front() == points.back())
+  {
+    points.pop_back();
+    close = true;
+  }
+
+// ... but there must be at least 3 points to make a closed path
+  if (points.size() < 3)
+    close = false;
+
+// need at least 2 points to make a polyline
+  if (points.size() < 2)
+    return path;
+
+  deque<Point>::const_iterator it = points.begin();
+  path->appendMoveTo(it->x, it->y);
+  ++it;
+  for (; it != points.end(); ++it)
+    path->appendLineTo(it->x, it->y);
+
+  if (close)
+    path->appendClose();
+
+  return path;
+}
+
 }
 
 KNObjectPtr_t makeObject(const KNShapePtr_t &shape)
@@ -27,6 +141,18 @@ KNObjectPtr_t makeObject(const KNShapePtr_t &shape)
   // TODO: implement me
   (void) shape;
   return KNObjectPtr_t();
+}
+
+KNPathPtr_t makePolygonPath(const KNSize &size, const unsigned edges)
+{
+  // user space canvas: [-1:1] x [-1:1]
+  const deque<Point> points = rotatePoint(Point(0, -1), edges);
+  const KNPathPtr_t path = makePolyLine(points);
+
+  const KNTransformation tr = translate(0, 1) * scale(0.5, 0.5) * scale(size.width, size.height);
+  path->transform(tr);
+
+  return path;
 }
 
 KNPathPtr_t makeRoundedRectanglePath(const KNSize &size, const double radius)
