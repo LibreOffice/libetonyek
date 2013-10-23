6b6262f3b616aa901f05c576ae748cc32d565d41
David Tardon <dtardon@redhat.com>
2013-10-23 12:54:57 +0200

    temporarily disable debug msgs.
    They interfere with output to stdout.

--- src/lib/libetonyek_utils.h
+++ src/lib/libetonyek_utils.h
@@ -60,6 +60,7 @@ typedef int64 int64_t;
 // debug message includes source file and line number
 //#define VERBOSE_DEBUG 1
 
+#undef DEBUG
 // do nothing with debug messages in a release compile
 #ifdef DEBUG
 #ifdef VERBOSE_DEBUG
