abde3dc1e9c56f7bf68c91279d80a350beb58784
David Tardon <dtardon@redhat.com>
2013-10-30 17:57:51 +0100

    fix windows build

--- src/lib/libetonyek_utils.h
+++ src/lib/libetonyek_utils.h
@@ -24,11 +24,11 @@
 typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned uint32_t;
-typedef unsigned int64 uint64_t;
+typedef unsigned __int64 uint64_t;
 typedef signed char int8_t;
 typedef short int16_t;
 typedef int int32_t;
-typedef int64 int64_t;
+typedef __int64 int64_t;
 
 #else
 
