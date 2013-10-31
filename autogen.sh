6fcc05adad479d9fd367e3336c1f6c8581d537aa
Fridrich Štrba <fridrich.strba@bluewin.ch>
2013-10-31 06:39:23 +0100

    add automatic git hook setting in autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,20 @@
 #!/bin/sh
+
+set_git_hooks()
+{
+    # assume that the current directory is the source tree
+    if [ -d ".git" ] ; then
+        for hook in $(ls -1 .git-hooks) ; do
+            cd .git/hooks
+            if [ ! -e "${hook?}" -o -L "${hook?}" ] ; then
+                rm -f "${hook?}"
+                ln -sf "../../.git-hooks/${hook?}" "${hook?}"
+            fi
+            cd - > /dev/null
+        done
+    fi
+}
+
 TESTLIBTOOLIZE="glibtoolize libtoolize"
 
 LIBTOOLIZEFOUND="0"
@@ -8,6 +24,8 @@ test -z "$srcdir" && srcdir=.
 
 olddir=`pwd`
 cd $srcdir
+
+set_git_hooks
 
 aclocal --version > /dev/null 2> /dev/null || {
     echo "error: aclocal not found"
