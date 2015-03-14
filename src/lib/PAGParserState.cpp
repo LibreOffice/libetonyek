d5c634a1bf69410f7991e2dd3847c0081dc7dbeb
David Tardon <dtardon@redhat.com>
2015-03-14 14:04:54 +0100

    define functions
    Change-Id: Ic72072cfa6a3784b2cc653f80e558bffae2051c7

---
+++ src/lib/PAGParserState.cpp
@@ -0,0 +1,30 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/*
+ * This file is part of the libetonyek project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include "PAGParserState.h"
+
+#include "PAGDictionary.h"
+
+namespace libetonyek
+{
+
+PAGParserState::PAGParserState(IWORKParser &parser, PAGDictionary &dict, const IWORKXMLReader::TokenizerFunction_t &tokenizer)
+  : IWORKXMLParserState(parser, dict, tokenizer)
+  , m_dict(dict)
+{
+}
+
+PAGDictionary &PAGParserState::getDictionary()
+{
+  return m_dict;
+}
+
+}
+
+/* vim:set shiftwidth=2 softtabstop=2 expandtab: */
