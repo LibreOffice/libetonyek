e0d6f22ac4cc5db6e88e05be18c79acf08d894c0
Anurag Kanungo <anuragkanungo@gmail.com>
2015-07-17 22:08:17 +0530

    refactor group element to enable group parsing in apple numbers format
    Change-Id: If4c882dd0049496d354d4dec44f64fa4212b51e5

---
+++ src/lib/contexts/IWORKGroupElement.cpp
@@ -0,0 +1,70 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/*
+ * This file is part of the libetonyek project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include "IWORKGroupElement.h"
+
+#include "IWORKCollector.h"
+#include "IWORKGeometryElement.h"
+#include "IWORKImageElement.h"
+#include "IWORKLineElement.h"
+#include "IWORKMediaElement.h"
+#include "IWORKShapeContext.h"
+#include "IWORKToken.h"
+#include "IWORKXMLParserState.h"
+
+namespace libetonyek
+{
+
+IWORKGroupElement::IWORKGroupElement(IWORKXMLParserState &state)
+  : IWORKXMLElementContextBase(state)
+{
+}
+
+void IWORKGroupElement::startOfElement()
+{
+  if (isCollector())
+  {
+    getCollector().startLevel();
+    getCollector().startGroup();
+  }
+}
+
+IWORKXMLContextPtr_t IWORKGroupElement::element(const int name)
+{
+  switch (name)
+  {
+  case IWORKToken::NS_URI_SF | IWORKToken::geometry :
+    return makeContext<IWORKGeometryElement>(getState());
+  case IWORKToken::NS_URI_SF | IWORKToken::group :
+    return makeContext<IWORKGroupElement>(getState());
+  case IWORKToken::NS_URI_SF | IWORKToken::image :
+    return makeContext<IWORKImageElement>(getState());
+  case IWORKToken::NS_URI_SF | IWORKToken::line :
+    return makeContext<IWORKLineElement>(getState());
+  case IWORKToken::NS_URI_SF | IWORKToken::media :
+    return makeContext<IWORKMediaElement>(getState());
+  case IWORKToken::NS_URI_SF | IWORKToken::shape :
+    return makeContext<IWORKShapeContext>(getState());
+  }
+
+  return IWORKXMLContextPtr_t();
+}
+
+void IWORKGroupElement::endOfElement()
+{
+  if (isCollector())
+  {
+    getCollector().endGroup();
+    getCollector().endLevel();
+  }
+}
+
+}
+
+/* vim:set shiftwidth=2 softtabstop=2 expandtab: */
---
+++ src/lib/contexts/IWORKImageElement.cpp
@@ -0,0 +1,71 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/*
+ * This file is part of the libetonyek project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include "IWORKImageElement.h"
+
+#include "libetonyek_xml.h"
+#include "IWORKCollector.h"
+#include "IWORKDictionary.h"
+#include "IWORKGeometryElement.h"
+#include "IWORKToken.h"
+#include "IWORKXMLParserState.h"
+
+namespace libetonyek
+{
+
+IWORKImageElement::IWORKImageElement(IWORKXMLParserState &state)
+  : IWORKXMLElementContextBase(state)
+  , m_image(new IWORKImage())
+{
+}
+
+void IWORKImageElement::startOfElement()
+{
+  if (isCollector())
+    getCollector().startLevel();
+}
+
+void IWORKImageElement::attribute(const int name, const char *const value)
+{
+  switch (name)
+  {
+  case IWORKToken::NS_URI_SF | IWORKToken::locked :
+    m_image->m_locked = bool_cast(value);
+    break;
+  default :
+    IWORKXMLElementContextBase::attribute(name, value);
+    break;
+  }
+}
+
+IWORKXMLContextPtr_t IWORKImageElement::element(const int name)
+{
+  switch (name)
+  {
+  case IWORKToken::NS_URI_SF | IWORKToken::geometry :
+    return makeContext<IWORKGeometryElement>(getState());
+  }
+
+  return IWORKXMLContextPtr_t();
+}
+
+void IWORKImageElement::endOfElement()
+{
+  if (getId())
+    getState().getDictionary().m_images[get(getId())] = m_image;
+  if (isCollector())
+  {
+    getCollector().collectImage(m_image);
+    getCollector().endLevel();
+  }
+}
+
+}
+
+/* vim:set shiftwidth=2 softtabstop=2 expandtab: */
