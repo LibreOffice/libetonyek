30ad10e89f3d0b63235ed176553d95d026ad7fa2
David Tardon <dtardon@redhat.com>
2015-09-10 16:01:15 +0200

    add unit test for Snappy decompression
    Change-Id: I7f844c423bb23d2dd5c448d45ff241de9e9f44c5

--- src/test/IWASnappyStreamTest.cpp
+++ src/test/IWASnappyStreamTest.cpp
@@ -7,6 +7,9 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
+#include <algorithm>
+#include <string>
+
 #include <boost/make_shared.hpp>
 
 #include <cppunit/TestFixture.h>
@@ -17,18 +20,39 @@
 
 using namespace libetonyek;
 
+using std::string;
+
 namespace test
 {
 
 namespace
 {
 
-#if 0
-RVNGInputStreamPtr_t makeStream(const char *const bytes, const size_t len)
+void assertCompressed(const string &message, const unsigned char *const expected, const size_t expectedSize, const unsigned char *const compressed, const size_t compressedSize)
 {
-  return boost::make_shared<IWORKMemoryStream>(reinterpret_cast<const unsigned char *>(bytes), len);
+  const RVNGInputStreamPtr_t stream(new IWORKMemoryStream(compressed, compressedSize));
+  IWASnappyStream uncompressedStream(stream);
+  unsigned long uncompressedSize = 0;
+  const unsigned char *const uncompressed = uncompressedStream.read(expectedSize, uncompressedSize);
+  CPPUNIT_ASSERT_EQUAL_MESSAGE(message + ": size", expectedSize, uncompressedSize);
+  CPPUNIT_ASSERT_MESSAGE(message + ": input exhausted", uncompressedStream.isEnd());
+  CPPUNIT_ASSERT_MESSAGE(message + ": content", std::equal(expected, expected + expectedSize, uncompressed));
 }
-#endif
+
+void assertAnyException(const string &message, const unsigned char *const compressed, const size_t compressedSize)
+{
+  const RVNGInputStreamPtr_t stream(new IWORKMemoryStream(compressed, compressedSize));
+  bool exception = false;
+  try
+  {
+    IWASnappyStream uncompressedStream(stream);
+  }
+  catch (...)
+  {
+    exception = true;
+  }
+  CPPUNIT_ASSERT_MESSAGE(message, exception);
+}
 
 }
 
@@ -40,9 +64,13 @@ public:
 
 private:
   CPPUNIT_TEST_SUITE(IWASnappyStreamTest);
+  CPPUNIT_TEST(testSimple);
+  CPPUNIT_TEST(testInvalid);
   CPPUNIT_TEST_SUITE_END();
 
 private:
+  void testSimple();
+  void testInvalid();
 };
 
 void IWASnappyStreamTest::setUp()
@@ -52,6 +80,38 @@ void IWASnappyStreamTest::setUp()
 void IWASnappyStreamTest::tearDown()
 {
 }
+
+#define BYTES(b) (reinterpret_cast<const unsigned char *>(b)), (sizeof(b) - 1)
+
+void IWASnappyStreamTest::testSimple()
+{
+  // assertCompressed("empty", BYTES(""), BYTES("\x1\x0"));
+  assertCompressed("literal run", BYTES("a"), BYTES("\x3\x1\x0\x61"));
+  assertCompressed("literal run", BYTES("ab"), BYTES("\x4\x2\x4\x61\x62"));
+  assertCompressed("long literal run", BYTES("ab"), BYTES("\x5\x2\xf0\x1\x61\x62"));
+  assertCompressed("very long literal run", BYTES("ab"), BYTES("\x5\x2\xf4\x1\x0\x61\x62"));
+  assertCompressed("extra long literal run", BYTES("ab"), BYTES("\x5\x2\xf8\x1\x0\x0\x61\x62"));
+  assertCompressed("near reference",
+                   BYTES("abcdabcd"), BYTES("\x8\x8\xc\x61\x62\x63\x64\x1\x4"));
+  assertCompressed("near reference of length 5",
+                   BYTES("abcdeabcde"), BYTES("\x9\xa\x10\x61\x62\x63\x64\x65\x5\x5"));
+  assertCompressed("near reference to the middle of a run",
+                   BYTES("abcdefbcde"), BYTES("\xa\xa\x14\x61\x62\x63\x64\x65\x66\x1\x5"));
+  assertCompressed("repeated near reference",
+                   BYTES("abcbcbcb"), BYTES("\x7\x8\x8\x61\x62\x63\x5\x2"));
+  assertCompressed("far reference", BYTES("aa"), BYTES("\x6\x2\x0\x61\x2\x1\x0"));
+  assertCompressed("far reference of length 2",
+                   BYTES("abab"), BYTES("\x7\x4\x4\x61\x62\x6\x2\x0"));
+}
+
+void IWASnappyStreamTest::testInvalid()
+{
+  assertAnyException("Too short literal run", BYTES("\x3\x4\x10\x61"));
+  assertAnyException("Near reference without any data", BYTES("\x3\x1\x5\x0"));
+  assertAnyException("Far reference without any data", BYTES("\x4\x1\x2\x1\x0"));
+}
+
+#undef BYTES
 
 CPPUNIT_TEST_SUITE_REGISTRATION(IWASnappyStreamTest);
 
