bb674410917422d23ce558ed0240c51a0a1fa726
David Tardon <dtardon@redhat.com>
2015-09-30 15:37:03 +0200

    add generic binary format parser
    Change-Id: I7f274dce3daac5dbee39dc2112c0f4149e9200bb

---
+++ src/lib/IWAParser.cpp
@@ -0,0 +1,140 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/*
+ * This file is part of the libetonyek project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include "IWAParser.h"
+
+#include <cassert>
+#include <deque>
+
+#include <boost/optional.hpp>
+
+#include "IWAMessage.h"
+
+namespace libetonyek
+{
+
+using boost::optional;
+
+using std::deque;
+using std::make_pair;
+using std::pair;
+using std::string;
+
+IWAParser::ObjectRecord::ObjectRecord(const RVNGInputStreamPtr_t &stream, const unsigned type,
+                                      const long pos, const unsigned long headerLen, const unsigned long dataLen)
+  : m_stream(stream)
+  , m_type(type)
+  , m_headerRange(pos, pos + long(headerLen))
+  , m_dataRange(m_headerRange.second, m_headerRange.second + long(dataLen))
+{
+}
+
+IWAParser::IWAParser(const RVNGInputStreamPtr_t &fragments, const RVNGInputStreamPtr_t &package, IWORKCollector &collector)
+  : m_fragments(fragments)
+  , m_package(package)
+  , m_collector(collector)
+{
+}
+
+bool IWAParser::parse()
+{
+  parseObjectIndex();
+  return parseDocument();
+}
+
+boost::optional<IWAMessage> IWAParser::queryObject(const unsigned id, const unsigned type) const
+{
+  const RecordMap_t::const_iterator recIt = m_fragmentObjectMap.find(id);
+  if (recIt == m_fragmentObjectMap.end())
+    return boost::none;
+  if (!recIt->second.second.m_stream)
+  {
+    // scan the fragment file
+    const FileMap_t::iterator fragmentIt = m_fragmentMap.find(recIt->second.first);
+    if (fragmentIt == m_fragmentMap.end())
+      return boost::none;
+
+    if (!m_fragments->existsSubStream(fragmentIt->second.first.c_str()))
+    {
+      ETONYEK_DEBUG_MSG(("IWAParser::queryObject: file %s does not exist\n", fragmentIt->second.first.c_str()));
+      m_fragmentMap.erase(fragmentIt); // avoid unnecessary repeats of the lookup
+      return boost::none;
+    }
+    const RVNGInputStreamPtr_t stream(m_fragments->getSubStreamByName(fragmentIt->second.first.c_str()));
+    assert(bool(stream));
+    assert(!fragmentIt->second.second); // this could only happen if the fragment file had already been scanned
+    fragmentIt->second.second = stream;
+    const_cast<IWAParser *>(this)->scanFragment(fragmentIt->first, fragmentIt->second.second);
+  }
+
+  if (!recIt->second.second.m_stream)
+    return boost::none;
+  const ObjectRecord &objRecord = recIt->second.second;
+  if ((objRecord.m_type == type) || (type == 0))
+    return IWAMessage(objRecord.m_stream, objRecord.m_dataRange.first, objRecord.m_dataRange.second);
+  return boost::none;
+}
+
+void IWAParser::parseObjectIndex()
+{
+  if (!m_fragments->existsSubStream("Index/Metadata.iwa"))
+  {
+    ETONYEK_DEBUG_MSG(("Index/Metadata.iwa missing, can't create object index"));
+    return;
+  }
+
+  const RVNGInputStreamPtr_t input(m_fragments->getSubStreamByName("Index/Metadata.iwa"));
+}
+
+void IWAParser::scanFragment(const unsigned id, const RVNGInputStreamPtr_t &stream)
+{
+  try
+  {
+    while (!stream->isEnd())
+    {
+      // scan a single object
+      const uint64_t headerLen = readUVar(stream);
+      const long start = stream->tell();
+      const IWAMessage header(stream, headerLen);
+      if (!header.message(2) || !header.message(2).uint64(1))
+        break;
+      const uint64_t dataLen = header.message(2).uint64(1).get();
+      if (header.uint32(1))
+      {
+        const RecordMap_t::iterator recIt = m_fragmentObjectMap.find(header.uint32(1).get());
+        const optional<unsigned> type = header.uint32(1);
+        if (recIt != m_fragmentObjectMap.end())
+          recIt->second.second = ObjectRecord(stream, get_optional_value_or(type, 0), start, long(headerLen), long(dataLen));
+      }
+      if (stream->seek(start + long(headerLen) + long(dataLen), librevenge::RVNG_SEEK_CUR) != 0)
+        break;
+    }
+  }
+  catch (...)
+  {
+    // just read as much as possible
+  }
+
+  // remove all objects from the fragment that have not been found
+  RecordMap_t::iterator it = m_fragmentObjectMap.begin();
+  while (it != m_fragmentObjectMap.end())
+  {
+    const RecordMap_t::iterator curIt = it;
+    ++it;
+    if ((curIt->second.first == id) && !curIt->second.second.m_stream)
+    {
+      ETONYEK_DEBUG_MSG(("IWAParser::scanFragment: object with ID %u was not found\n", curIt->first));
+      m_fragmentObjectMap.erase(curIt);
+    }
+  }
+}
+
+}
+
+/* vim:set shiftwidth=2 softtabstop=2 expandtab: */
---
+++ src/lib/IWAParser.h
@@ -0,0 +1,77 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/*
+ * This file is part of the libetonyek project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef IWAPARSER_H_INCLUDED
+#define IWAPARSER_H_INCLUDED
+
+#include <map>
+#include <string>
+
+#include <boost/optional.hpp>
+#include <boost/unordered_map.hpp>
+
+#include "libetonyek_utils.h"
+
+namespace libetonyek
+{
+
+class IWAMessage;
+class IWORKCollector;
+
+class IWAParser
+{
+  // disable copying
+  IWAParser(const IWAParser &);
+  IWAParser &operator=(const IWAParser &);
+
+public:
+  struct ObjectRecord
+  {
+    ObjectRecord(const RVNGInputStreamPtr_t &stream, unsigned type, long pos, unsigned long headerLen, unsigned long dataLen);
+
+    RVNGInputStreamPtr_t m_stream;
+    unsigned m_type;
+    std::pair<long, long> m_headerRange;
+    std::pair<long, long> m_dataRange;
+  };
+
+  typedef std::map<unsigned, std::pair<std::string, RVNGInputStreamPtr_t> > FileMap_t;
+  typedef std::map<unsigned, std::pair<unsigned, ObjectRecord> > RecordMap_t;
+
+public:
+  IWAParser(const RVNGInputStreamPtr_t &fragments, const RVNGInputStreamPtr_t &package, IWORKCollector &collector);
+
+  bool parse();
+
+protected:
+  boost::optional<IWAMessage> queryObject(unsigned id, unsigned type = 0) const;
+
+private:
+  virtual bool parseDocument() = 0;
+
+private:
+  void parseObjectIndex();
+
+  void scanFragment(unsigned id, const RVNGInputStreamPtr_t &stream);
+
+private:
+  const RVNGInputStreamPtr_t m_fragments;
+  const RVNGInputStreamPtr_t m_package;
+  IWORKCollector &m_collector;
+
+  mutable FileMap_t m_fragmentMap;
+  mutable RecordMap_t m_fragmentObjectMap;
+  FileMap_t m_fileMap;
+};
+
+}
+
+#endif
+
+/* vim:set shiftwidth=2 softtabstop=2 expandtab: */
