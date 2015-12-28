4c809065aea31d260aa5f48d665b4ad6bea70b4f
David Tardon <dtardon@redhat.com>
2015-12-28 13:14:10 +0100

    allow to handle arrays of refs too
    Change-Id: I9e60a8d2de8691359aa0acb7dd3cf4b961a915fc

--- src/lib/contexts/IWORKContainerContext.h
+++ src/lib/contexts/IWORKContainerContext.h
@@ -12,17 +12,31 @@
 
 #include <deque>
 
+#include <boost/optional.hpp>
+
+#include "IWORKTypes_fwd.h"
+#include "IWORKRefContext.h"
 #include "IWORKXMLContextBase.h"
 
 namespace libetonyek
 {
 
-template<typename Type, class NestedParser, unsigned Id>
+template<typename Type, class NestedParser, unsigned Id, unsigned RefId = 0>
 class IWORKContainerContext : public IWORKXMLElementContextBase
 {
+  typedef boost::unordered_map<ID_t, Type> Dict_t;
+
 public:
   IWORKContainerContext(IWORKXMLParserState &state, std::deque<Type> &elements)
     : IWORKXMLElementContextBase(state)
+    , m_dict(0)
+    , m_elements(elements)
+  {
+  }
+
+  IWORKContainerContext(IWORKXMLParserState &state, Dict_t &dict, std::deque<Type> &elements)
+    : IWORKXMLElementContextBase(state)
+    , m_dict(&dict)
     , m_elements(elements)
   {
   }
@@ -30,12 +44,36 @@ public:
 private:
   virtual IWORKXMLContextPtr_t element(const int name)
   {
+    if (m_ref)
+      handleRef();
+
     if (name == Id)
       return makeContext<NestedParser>(getState(), m_elements);
+    else if ((RefId != 0) && (name == RefId))
+      return makeContext<IWORKRefContext>(getState(), m_ref);
     return IWORKXMLContextPtr_t();
   }
 
+  virtual void endOfElement()
+  {
+    if (m_ref)
+      handleRef();
+  }
+
+  void handleRef()
+  {
+    assert(m_dict);
+
+    const typename Dict_t::const_iterator it = m_dict->find(get(m_ref));
+    if (it == m_dict->end())
+      m_elements.push_back(Type());
+    else
+      m_elements.push_back(it->second);
+  }
+
 private:
+  Dict_t *const m_dict;
+  boost::optional<ID_t> m_ref;
   std::deque<Type> &m_elements;
 };
 
